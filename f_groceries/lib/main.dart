b3d9ad431988f898c2424ef95c585e7f29a0fd80
gerites <seshukumar.pasineni@gmail.com>
2019-03-14 18:25:40 +0530

    initializing with an empty light theme

--- f_groceries/lib/main.dart
+++ f_groceries/lib/main.dart
@@ -7,24 +7,17 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  ThemeData _buildSampleTheme() {
+    final ThemeData base =ThemeData.light();
+    return base;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final ThemeData _sampleTheme = _buildSampleTheme();
     return new MaterialApp(
-        theme: new ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-          // counter didn't reset back to zero; the application is not restarted.
-
-            primaryColor: Colors.white,
-            primaryColorDark: Colors.white30,
-            accentColor: Colors.blue
-
-        ),
+        theme: _sampleTheme,
       home: new MyHomePage(title: 'Groceries'),
     );
   }
